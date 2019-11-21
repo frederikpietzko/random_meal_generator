2944e84992a5c7ce30d67a54f66994204b5b1173
Frederik Pietzko <pietzkofred@gmail.com>
2019-11-21 12:40:56 +0100

    build

---
+++ client/build/precache-manifest.a78b0a01ae0e08db4d4e8c7eae53e653.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb097bca89e7f8509e7cda95d8bf6ea7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a26a4e8f3c6ff8380fa",
+    "url": "/static/css/main.525eb31e.chunk.css"
+  },
+  {
+    "revision": "3b7baea14a6b391b9a84",
+    "url": "/static/js/2.7012d4c9.chunk.js"
+  },
+  {
+    "revision": "8a26a4e8f3c6ff8380fa",
+    "url": "/static/js/main.0686f92b.chunk.js"
+  },
+  {
+    "revision": "ca0acbb00541b7084e1f",
+    "url": "/static/js/runtime-main.67ac8bf8.js"
+  },
+  {
+    "revision": "62b4d768abb37bfc7b6ae332a41d289f",
+    "url": "/static/media/Hintergrund2.62b4d768.svg"
+  }
+]);
